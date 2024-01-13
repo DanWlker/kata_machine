d2900c7ad312d5cf5e17b6c1ba5ce2eed9e0ba8c
DanWlker <danielhee2@gmail.com>
2024-01-13 16:34:14 +0800

    bubble sort

--- lib/bubble_sort.dart
+++ lib/bubble_sort.dart
@@ -1,1 +1,18 @@
-void bubbleSort(List<int> arr) {}
+void bubbleSort(List<int> arr) {
+  /// for i..n
+  ///   for j..n-1-i
+  ///     if arr[i] > arr[j]
+  ///       swap(i, j)
+  ///
+
+  for (int i = 0; i < arr.length; ++i) {
+    //up to but not include the last element cuz we going to do a +1 check
+    for (int j = 0; j < arr.length - 1 - i; ++j) {
+      if (arr[j] > arr[j + 1]) {
+        final tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
+}
--- test/lib/bubble_sort_test.dart
+++ test/lib/bubble_sort_test.dart
@@ -3,7 +3,7 @@ import 'package:test/test.dart';
 
 void bubbleSortTest() {
   test("bubble-sort", () {
-    const arr = [9, 3, 7, 4, 69, 420, 42];
+    final arr = [9, 3, 7, 4, 69, 420, 42];
 
     bubbleSort(arr);
     expect(arr, [3, 4, 7, 9, 42, 69, 420]);
